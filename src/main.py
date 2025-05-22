e08b6af5dc3457f44e7c565b6b20db7358ded4e6
vyacheslav <vektrik@ya.ru>
2025-05-22 19:49:39 +0500

    added datefomatting func

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
+import datetime
 from src.masks import get_reqiusits
 
 if __name__ == "__main__":
-    with open("../tests/accounts_cards.txt", "r", encoding= 'utf-8') as file:
+    with open("../tests/accounts_cards.txt", "r", encoding="utf-8") as file:
         lines = file.readlines()
         for line in lines:
             req_mask = get_reqiusits(line)
