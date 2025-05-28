23d8d870b200190816c5b2c12a00cf39b7140c03
vyacheslav <vektrik@ya.ru>
2025-05-28 22:24:20 +0500

    added prototypes of functions

---
+++ src/processing.py
@@ -0,0 +1,13 @@
+def filer_by_state(my_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция принимает на вход список словарей и возвращает
+         только указанные в ключе state"""
+
+    return new_list
+
+
+def sort_by_date(my_list: list[dict], direction: bool = 1) -> list[dict]:
+    """Функция сортировки по дате"""
+
+    return new_list
+
+
