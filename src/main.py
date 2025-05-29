f7e0f17d63f90cf8b9139ef9b80a6f8d07defa45
vyacheslav <vektrik@ya.ru>
2025-05-29 22:03:50 +0500

    added readme.md file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from src.widget import get_reqiusits, get_date
-from  src.processing import filter_by_state, sort_by_date
+from src.processing import filter_by_state, sort_by_date
 
 if __name__ == "__main__":
     with open("../tests/accounts_cards.txt", "r", encoding="utf-8") as file:
@@ -8,10 +8,14 @@ if __name__ == "__main__":
             req_mask = get_reqiusits(line)
             print(req_mask)
 
-    my_list = filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                              {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}])
+    my_list = filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
     print(my_list)
     print(sort_by_date(my_list))
 
--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,13 @@
 from typing import Any, Dict, List
 
+
 def filter_by_state(my_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция принимает на вход список словарей и возвращает
-         только указанные в ключе state"""
+    только указанные в ключе state"""
     return [my_list for my_list in my_list if my_list.get("state") == state]
 
 
 def sort_by_date(operation_list: List[Dict[str, Any]], direction: bool = True) -> list[Dict[str, Any]]:
     """Функция сортировки по дате"""
 
-    return sorted(operation_list, key=lambda item: item['date'], reverse = direction)
-
-
+    return sorted(operation_list, key=lambda item: item["date"], reverse=direction)
