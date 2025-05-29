045285acdb37fa2ba01a52c9e85f78186674d202
vyacheslav <vektrik@ya.ru>
2025-05-29 20:03:53 +0500

    added filter_by_state func realization

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,7 @@
-def filer_by_state(my_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+def filter_by_state(my_list: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция принимает на вход список словарей и возвращает
          только указанные в ключе state"""
-
-    return new_list
+    return [my_list for my_list in my_list if my_list.get("state") == state]
 
 
 def sort_by_date(my_list: list[dict], direction: bool = 1) -> list[dict]:
