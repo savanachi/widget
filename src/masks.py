6daefa637c9d3eed4d502aadf02bb47d0c551533
vyacheslav <vektrik@ya.ru>
2025-06-03 21:32:48 +0500

    initial setting for home_work_10_2

---
+++ src/masks.py
@@ -0,0 +1,22 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция принимает на вход номер карты и маскирует цифры номера."""
+    card_list = list(card_number)
+    for index in range(len(card_number)):
+        if 6 <= index <= 11:
+            card_list[index] = "*"
+
+    masked_str = "".join(card_list)
+    card_str = " ".join(masked_str[i : i + 4] for i in range(0, len(masked_str), 4))
+
+    return card_str
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция принимает на вход номер счёта и возвращает маску."""
+    masked_acc = list(account_number[-6:])
+    for i in range(0, 2):
+        masked_acc[i] = "*"
+
+    masked_str = "".join(masked_acc)
+
+    return masked_str
